1f26ab266c9802a51f796be6d68de01a3720ea10
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-10-19 20:57:23 +0700

    Update mini_3.py

--- mini/mini_3.py
+++ mini/mini_3.py
@@ -1,5 +1,10 @@
 def solve(s):
-    print([list(map(float, x.split())) for x in list(s.split("|"))])
+    return [list(map(float, x.split())) for x in list(s.split("|"))]
 
 
-solve("1 2 | 3 4")
+assert solve("1 2 | 3 4") == [[1.0, 2.0], [3.0, 4.0]], "Wrong answer"
+assert solve("") == [[]], "Wrong answer"
+assert solve("1") == [[1.0]], "Wrong answer"
+assert solve("1 | 2 2 | 333") == [[1.0], [2.0, 2.0], [333.0]], "Wrong answer"
+
+print("ALL TESTS PASSED :D")
