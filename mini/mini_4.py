a60a3645599c00e88a7eb0c1a20df73ae45b7faa
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-10-20 00:20:22 +0700

    Update mini_4.py

--- mini/mini_4.py
+++ mini/mini_4.py
@@ -5,15 +5,17 @@ def solve(d):
 
     for i in range(len(a)):
         if b[i] in new_d.keys():
-            new_d[b[i]] = new_d[b[i]] + tuple([a[i]])
+            if isinstance(new_d[b[i]], str):
+                new_d[b[i]] = [new_d[b[i]]]
+            new_d[b[i]] = tuple(list(new_d[b[i]]) + [a[i]])
         else:
-            new_d[b[i]] = tuple([a[i]])
+            new_d[b[i]] = a[i]
 
     return new_d
 
 
-assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Kuznecov2": 97832}) == {97832: ('Ivanov', 'Kuznecov', 'Kuznecov2'), 55521: ('Petrov',)}, "Wrong answer"
-assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ('Ivanov', 'Kuznecov'), 55521: ('Petrov',)}, "Wrong answer"
-assert solve({"Ivanov": 97832, "Petrov": 55521}) == {97832: ('Ivanov',), 55521: ('Petrov',)}, "Wrong answer"
+assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Kuznecov2": 97832}) == {97832: ('Ivanov', 'Kuznecov', 'Kuznecov2'), 55521: 'Petrov'}, "Wrong answer"
+assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ('Ivanov', 'Kuznecov'), 55521: 'Petrov'}, "Wrong answer"
+assert solve({"Ivanov": 97832, "Petrov": 55521}) == {97832: 'Ivanov', 55521: 'Petrov'}, "Wrong answer"
 
 print("ALL TESTS PASSED :D")
