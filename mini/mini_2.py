a449ca8e8f4ceacfe4950c39901ee2fe8a44ff8c
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-10-19 20:57:07 +0700

    Update mini_2.py

--- mini/mini_2.py
+++ mini/mini_2.py
@@ -1,5 +1,11 @@
-def solve(a, b):
-    print(list((a[i], b[i]) for i in range((min(len(a), len(b))))))
+def my_zip(a, b):
+    return [(a[i], b[i]) for i in range(min(len(a), len(b)))]
 
 
-solve([1, 2, 3], ["a", "b"])
+assert my_zip([1, 2, 3], ["a", "b"]) == [(1, 'a'), (2, 'b')], "Wrong answer"
+assert my_zip([], []) == [], "Wrong answer"
+assert my_zip([], [1, 2]) == [], "Wrong answer"
+assert my_zip([1], []) == [], "Wrong answer"
+assert my_zip([1, 22], ["dasdf", "dfs"]) == [(1, 'dasdf'), (22, 'dfs')], "Wrong answer"
+
+print("ALL TESTS PASSED :D")
