170d1980c6c0f261f8e484daafa23ffef6878b04
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-10-19 20:57:35 +0700

    Update mini_4.py

--- mini/mini_4.py
+++ mini/mini_4.py
@@ -5,11 +5,15 @@ def solve(d):
 
     for i in range(len(a)):
         if b[i] in new_d.keys():
-            new_d[b[i]] = tuple([new_d[b[i]]]) + tuple([a[i]])
+            new_d[b[i]] = new_d[b[i]] + tuple([a[i]])
         else:
-            new_d[b[i]] = a[i]
+            new_d[b[i]] = tuple([a[i]])
 
-    print(new_d)
+    return new_d
 
 
-solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832})
+assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Kuznecov2": 97832}) == {97832: ('Ivanov', 'Kuznecov', 'Kuznecov2'), 55521: ('Petrov',)}, "Wrong answer"
+assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ('Ivanov', 'Kuznecov'), 55521: ('Petrov',)}, "Wrong answer"
+assert solve({"Ivanov": 97832, "Petrov": 55521}) == {97832: ('Ivanov',), 55521: ('Petrov',)}, "Wrong answer"
+
+print("ALL TESTS PASSED :D")
