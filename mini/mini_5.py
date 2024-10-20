3c92e221dc2ebf5ac0649b2259b10c72b11c3c4a
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-10-20 10:51:43 +0700

    mini_5

---
+++ mini/mini_5.py
@@ -0,0 +1,21 @@
+def specialize(f, *a, **k):
+    def f_modifire(*args, **kwargs):
+        new_f = f(*a, *args, **k, **kwargs)
+        return new_f
+    return f_modifire
+
+
+def my_sum(x, y):
+    return x + y
+
+
+plus_one = specialize(my_sum, y=1)
+just_two = specialize(my_sum, 1, 1)
+
+assert plus_one(10) == 11, "Wrong answer"
+assert just_two() == 2, "Wrong answer"
+assert specialize(str, 123)() == "123", "Wrong answer"
+assert list(specialize(map, int)(["1", "2"])) == [1, 2], "Wrong answer"
+
+
+print("ALL TESTS PASSED :D")
