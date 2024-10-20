855da8dc7e4cfa82568639dd619272bf86b70271
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-10-20 10:51:57 +0700

    Update mini_4.py

--- mini/mini_4.py
+++ mini/mini_4.py
@@ -5,7 +5,7 @@ def solve(d):
 
     for i in range(len(a)):
         if b[i] in new_d.keys():
-            if isinstance(new_d[b[i]], str):
+            if not isinstance(new_d[b[i]], tuple):
                 new_d[b[i]] = [new_d[b[i]]]
             new_d[b[i]] = tuple(list(new_d[b[i]]) + [a[i]])
         else:
@@ -17,5 +17,10 @@ def solve(d):
 assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Kuznecov2": 97832}) == {97832: ('Ivanov', 'Kuznecov', 'Kuznecov2'), 55521: 'Petrov'}, "Wrong answer"
 assert solve({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}) == {97832: ('Ivanov', 'Kuznecov'), 55521: 'Petrov'}, "Wrong answer"
 assert solve({"Ivanov": 97832, "Petrov": 55521}) == {97832: 'Ivanov', 55521: 'Petrov'}, "Wrong answer"
+assert solve({4: 3, 1: 2, 8: 2}) == {3: 4, 2: (1, 8)}, "Wrong answer"
+assert solve({4: 3, 1: 2}) == {3: 4, 2: 1}, "Wrong answer"
+assert solve({4: 3}) == {3: 4}, "Wrong answer"
+assert solve({}) == {}, "Wrong answer"
+
 
 print("ALL TESTS PASSED :D")
