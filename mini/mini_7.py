34457f7683ee5cdb4fa7d286b720b7ea6dbcfb78
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-11-04 15:15:03 +0700

    Update mini_7.py

--- mini/mini_7.py
+++ mini/mini_7.py
@@ -5,20 +5,20 @@ def deprecated(f=None, *, since=None, will_be_removed=None):
     if f is None:
         return partial(deprecated, since=since, will_be_removed=will_be_removed)
 
-    print(f"Warning: function {f.__name__} is deprecated", end="")
-
-    if since is not None:
-        print(f" since version{since}", end="")
-
-    print(". It will be removed in ", end="")
-
-    if will_be_removed is None:
-        print(f"future versions.")
-    else:
-        print(f"version {will_be_removed}.")
-
     @wraps(f)
     def inner(*args, **kwargs):
+        print(f"Warning: function {f.__name__} is deprecated", end="")
+
+        if since is not None:
+            print(f" since version {since}", end="")
+
+        print(". It will be removed in ", end="")
+
+        if will_be_removed is None:
+            print(f"future versions.")
+        else:
+            print(f"version {will_be_removed}.")
+
         ret = f(*args, **kwargs)
         return ret
 
