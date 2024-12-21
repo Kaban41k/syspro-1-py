07057ca3e608be71298b2b879f9df2501381fc0f
Aleksandr Anishchenko <74061596+Kaban41k@users.noreply.github.com>
2024-12-21 16:02:26 +0700

    Update mini_10.py

--- mini/mini_10.py
+++ mini/mini_10.py
@@ -2,21 +2,35 @@ def singleton(class_to_decorate):
     class SingletonClass(class_to_decorate):
         instance = None
 
-        def __new__(cls):
+        def __new__(cls, *args, **kwargs):
             if SingletonClass.instance is None:
                 SingletonClass.instance = super().__new__(cls)
             return SingletonClass.instance
+
+        def __init__(self, *args, **kwargs):
+            try:
+                if self._inited:
+                    return
+            except:
+                self._inited = True
+
+            super().__init__(*args, **kwargs)
     return SingletonClass
 
 
 @singleton
-class Node():
+class Node:
+    def __init__(self, x):
+        self.x = x
+        print("Hello from init")
     pass
 
 
-a = Node()
-b = Node()
+a = Node(1)
+b = Node(2)
 
 print(a)
 print(b)
-print(a is b)
+assert a is b, "Wrong answer"
+
+print("ALL TESTS PASSED :D")
